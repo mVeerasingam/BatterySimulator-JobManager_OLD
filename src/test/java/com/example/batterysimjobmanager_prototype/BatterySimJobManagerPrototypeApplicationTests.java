950a1c5196d38f97d383fbbd57f4fa1749386d89
Mark Veerasingam <markveer70@gmail.com>
2023-11-22 21:57:36 +0000

    First Commit. from previous rapid prototype

---
+++ src/test/java/com/example/batterysimjobmanager_prototype/BatterySimJobManagerPrototypeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.batterysimjobmanager_prototype;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatterySimJobManagerPrototypeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
